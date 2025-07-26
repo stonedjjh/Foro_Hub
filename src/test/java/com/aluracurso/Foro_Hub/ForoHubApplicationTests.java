21f4cd4451ae9d3231eb2358cfe59781e61be94c
stonedjjh <danjim82@gmail.com>
2025-07-26 15:10:13 -0400

    Estado inicial del proyecto

---
+++ src/test/java/com/aluracurso/Foro_Hub/ForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracurso.Foro_Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
